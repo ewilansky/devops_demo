0d354bfde4a4942b277feada74d100be1733a710
Ethan Wilansky <ewilansky@yahoo.com>
2018-10-01 08:18:16 -0400

    file cleanup and reorg

--- utils/gradleBootRun.sh
+++ utils/gradleBootRun.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+cd ../spring-boot-demo
 docker run --rm -v "$PWD":/home/gradle/project -w /home/gradle/project gradle gradle bootRun
--- utils/gradleTest.sh
+++ utils/gradleTest.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+cd ../spring-boot-demo
 docker run --rm -v "${PWD}/spring-boot-demo":/home/gradle/project -w /home/gradle/project gradle gradle clean test
