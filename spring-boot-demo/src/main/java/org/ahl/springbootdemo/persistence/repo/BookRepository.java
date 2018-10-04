bfb687904bef93c18ea6517e75e6851619215136
Ethan Wilansky <ewilansky@yahoo.com>
2018-10-04 14:23:18 -0400

    added repository annotatin

--- spring-boot-demo/src/main/java/org/ahl/springbootdemo/persistence/repo/BookRepository.java
+++ spring-boot-demo/src/main/java/org/ahl/springbootdemo/persistence/repo/BookRepository.java
@@ -3,7 +3,9 @@ package org.ahl.springbootdemo.persistence.repo;
 import java.util.List;
 import org.ahl.springbootdemo.persistence.model.Book;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends CrudRepository<Book, Long> {
 	List<Book> findByTitle(String title);
 }
