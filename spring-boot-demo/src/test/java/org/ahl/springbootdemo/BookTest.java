84c377cd3cae7707865b4b004682f9f48dde55c2
Ethan Wilansky <ewilansky@yahoo.com>
2018-10-03 21:54:53 -0400

    downgrade to junit 4 to support cucumber bdd

--- spring-boot-demo/src/test/java/org/ahl/springbootdemo/BookTest.java
+++ spring-boot-demo/src/test/java/org/ahl/springbootdemo/BookTest.java
@@ -1,27 +1,39 @@
 package org.ahl.springbootdemo;
 
-import static org.junit.jupiter.api.Assertions.*;
+// import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 
 import org.ahl.springbootdemo.persistence.model.Book;
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 
 public class BookTest {
 
 	@Test
-	void ShouldReturnBookTitleAndAuthor() {
+	public void ShouldReturnBookTitleAndAuthor() {
 		Book book = new Book("Abaddon's Gate", "James S. A. Corey");
-		assertEquals("Abaddon's Gate", book.getTitle(), "unable to return book title");
-		assertEquals("James S. A. Corey", book.getAuthor(), "unable to return author title");
+		assertEquals("unable to return book title", "Abaddon's Gate", book.getTitle());
+		assertEquals("unable to return author title", "James S. A. Corey", book.getAuthor());
 	}
 	
 	@Test
-	void ShouldSetAndGetIdBookTitleAndAuthor() {
+	public void ShouldSetAndGetIdBookTitleAndAuthor() {
 		Book book = new Book();
 		book.setAuthor("James S. A. Corey");
 		book.setTitle("Abaddon's Gate");
 		book.setId(12345);
-		assertEquals("Abaddon's Gate", book.getTitle(), "unable to return book title");
-		assertEquals("James S. A. Corey", book.getAuthor(), "unable to return author title");
-		assertEquals(12345, book.getId(), "unable to return book id");
+		assertEquals("unable to return book title", "Abaddon's Gate", book.getTitle());
+		assertEquals("unable to return author title", "James S. A. Corey", book.getAuthor());
+		// assertEquals(12345, book.getId(), "unable to return book id");
+		assertEquals(12345, book.getId());
+	}
+	
+	@Test
+	public void ShouldReturnAHashCode() {
+		Book book = new Book();
+		Integer code = book.hashCode();
+		
+		assertTrue(code.intValue() != 0);
 	}
 }
