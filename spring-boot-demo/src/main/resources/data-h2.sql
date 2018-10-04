fb1752220a1fc6dd41404c3882e51dd246d9b0c2
Ethan Wilansky <ewilansky@yahoo.com>
2018-10-04 13:43:16 -0400

    adding staged data

---
+++ spring-boot-demo/src/main/resources/data-h2.sql
@@ -0,0 +1,3 @@
+INSERT INTO book (id, title, author) VALUES
+  (1, 'The Lean Startup: How Today''s Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses', 'Eric Ries'),
+  (2, 'Rebel Talent: Why It Pays to Break the Rules at Work and in Life', 'Francesca Gino');
