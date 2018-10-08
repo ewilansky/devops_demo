bb66feb0cc1618e3f18a4859e9db78bc0c211f4b
Ethan Wilansky <ewilansky@yahoo.com>
2018-10-08 11:34:17 -0500

    updated test with unit test steps

--- spring-boot-demo/src/cucumberTest/java/org/ahl/springbootdemo/BookTestSteps.java
+++ spring-boot-demo/src/cucumberTest/java/org/ahl/springbootdemo/BookTestSteps.java
@@ -1,29 +1,32 @@
 package org.ahl.springbootdemo;
+import static org.junit.Assert.assertEquals;
+import org.ahl.springbootdemo.persistence.model.Book;
 
 import cucumber.api.java.en.*;
 
 public class BookTestSteps {
     @Given("The User creates a book")
     public void the_User_creates_a_book() {
-        // Write code here that turns the phrase above into concrete actions
-        System.out.format("A book was created");
+        Book book = new Book("Abaddon's Gate", "James S. A. Corey");
+        assertEquals("unable to return book title", "Abaddon's Gate", book.getTitle());
+		assertEquals("unable to return author title", "James S. A. Corey", book.getAuthor());
     }
 
     @Given("The title is {string}")
-    public void the_title_is(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        System.out.format("A title is set");
+    public void the_title_is(String title) {
+        Book book = new Book("Abaddon's Gate", "James S. A. Corey");
+        assertEquals("unable to return book title", title, book.getTitle());
     }
 
     @Given("The author is {string}")
-    public void the_author_is(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        System.out.format("An author is set");
+    public void the_author_is(String author) {
+        Book book = new Book("Abaddon's Gate", "James S. A. Corey");
+        assertEquals("unable to return book author", author, book.getAuthor());
     }
 
     @When("The user saves a book title and author")
     public void the_user_saves_a_book_title_and_author() {
-        // Write code here that turns the phrase above into concrete actions
+        // TODO: mock the db call and return a record with a mocked api call...
         System.out.format("User saved book title and author");
     }
 
